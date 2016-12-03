6303243f7a7a1c93197de5c6be2906e9b6c982c5
amjad83m <amjad83m@hotmail.com>
2016-12-03 04:21:43 +0100

    initial commit

---
+++ rijkstest.js
@@ -0,0 +1,28 @@
+// search the collection using a JSON call
+      function search(query) {
+        return $.getJSON("https://www.rijksmuseum.nl/api/nl/collection?q=Q&key=eBvLLJik&format=json".replace("Q", query));
+      }
+
+
+var searchBtn = document.getElementById("search");
+searchBtn.addEventListener("click", doSearch);
+
+var resultD = document.getElementById("result");
+var searchField = document.getElementById("query");
+
+function doSearch() {
+    resultD.innerHTML = "";
+    var searchString = searchField.value;
+    if (searchString !== "") {
+         search(searchString).done(function(data) {
+            for (let artObj in data.artObjects) {
+                var rImg = document.createElement("img");
+                rImg.src = data.artObjects[artObj].webImage.url;
+                resultD.appendChild(rImg);
+                resultD.innerHTML += data.artObjects[artObj].title;
+                resultD.innerHTML += "<br>&nbsp;<br>&nbsp;<br>&nbsp;<br>";
+            }
+        });
+    }
+}
+
